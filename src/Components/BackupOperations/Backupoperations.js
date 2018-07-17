ec55b42aadb575c206325ef9517395e90f49e3ba
uddish <uddish@squadplatform.com>
2018-07-17 17:25:41 +0200

    added pagination support in operations

--- src/Components/BackupOperations/Backupoperations.js
+++ src/Components/BackupOperations/Backupoperations.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import BackupoperationsItem from './BackupoperationsItem';
 import $ from 'jquery';
+import {ButtonToolbar, Button} from 'react-bootstrap';
 
 
 class Backupoperations extends Component  {
@@ -8,7 +9,11 @@ class Backupoperations extends Component  {
     super();
     this.state = {
       backupoperations: [],
+      pageCount: 1,
+      totalPageCount: 0,
     }
+    this.previousButtonClicked = this.previousButtonClicked.bind(this);
+    this.nextButtonClicked = this.nextButtonClicked.bind(this);
   }
 
   componentDidMount() {
@@ -18,12 +23,16 @@ class Backupoperations extends Component  {
   //Fetching backup operations from the API
   getBackupOperations() {
     $.ajax({
-      url: 'http://localhost:8000/backup-operations/3/',
+      url: 'http://localhost:8000/backup-operations/3/?page=' + (this.state.pageCount),
       dataType: 'json',
       cache: 'false',
       contentType: 'application/json',
       success: function(data) {
-        this.setState({backupoperations: data.results}, function() {
+        this.setState({
+          backupoperations: data.results,
+          totalPageCount: data.count,
+          }, function() {
+          this.calculateTotalPageCount()
           console.log(this.state);
         })
       }.bind(this),
@@ -31,6 +40,42 @@ class Backupoperations extends Component  {
         console.log(err);
       }
     })
+  }
+
+  calculateTotalPageCount() {
+    //If the value is a float, add 1 to it
+    if(this.state.totalPageCount % 10 != 0) {
+      this.setState({
+        totalPageCount: parseInt(this.state.totalPageCount/10) + 1
+      })
+    }
+    else {
+      this.setState({
+        totalPageCount: (this.state.totalPageCount/10)
+      })
+    }
+  }
+
+  previousButtonClicked() {
+    if(this.state.pageCount > 1)  {
+      this.setState({
+        pageCount: this.state.pageCount - 1
+      }, function() {
+        console.log('Decreasing page count ' + this.state.pageCount)
+        this.getBackupOperations()
+      }
+    )}
+  }
+
+  nextButtonClicked() {
+    if(this.state.pageCount < this.state.totalPageCount)  {
+      this.setState({
+        pageCount: this.state.pageCount + 1
+      }, function() {
+        console.log('Increasing page count ' + this.state.pageCount)
+        this.getBackupOperations()
+      }
+    )}
   }
 
   render()  {
@@ -46,17 +91,24 @@ class Backupoperations extends Component  {
              <div>
                <h1 className="title">Backup Operations</h1>
                <table>
-                <tr>
-                  <th>App Id</th>
-                  <th>BOID</th>
-                  <th>Backup Type</th>
-                  <th>Last Backup</th>
-                  <th>Start Time </th>
-                  <th>Completion Time</th>
-                  <th>Status Time</th>
-                </tr>
+                 <tbody>
+                  <tr>
+                    <th>App Id</th>
+                    <th>BOID</th>
+                    <th>Backup Type</th>
+                    <th>Last Backup</th>
+                    <th>Start Time </th>
+                    <th>Completion Time</th>
+                    <th>Status Time</th>
+                  </tr>
+                </tbody>
                   {backupoperationsItems}
               </table>
+              <ButtonToolbar>
+                <Button bsStyle="primary" bsSize="small" onClick= {this.previousButtonClicked}>&larr; Previous</Button>
+                <Button bsStyle="primary" bsSize="small" onClick= {this.nextButtonClicked}>Next &rarr;</Button>
+              </ButtonToolbar>
+              <h6>Page {this.state.pageCount} of {this.state.totalPageCount}</h6>
              </div>  
           </div>
         </div>

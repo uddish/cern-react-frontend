abc489078f3a97c24f26fb5d8ab4cb656ef0b872
uddish <uddish@squadplatform.com>
2018-07-19 14:50:57 +0200

    last backup UI tweaks

--- src/Components/LastBackupOperation/LastBackupItem.js
+++ src/Components/LastBackupOperation/LastBackupItem.js
@@ -10,22 +10,9 @@ class LastBackupItem extends Component  {
           <div className="container-fluid">
             <div className="row">
               <div className="col-xs-4">
-                <h3>App Id</h3>
-                <h4 class="home-application-data">{this.props.lastBackup.appid}</h4>
-              </div>
-              <div className="col-xs-4">
-                <h3>Bo Id</h3>
-                <h4 class="home-application-data">{this.props.lastBackup.boid}</h4>
-              </div>
-              <div className="col-xs-4">
                 <h3>Backup Type</h3>
                 <h4 class="home-application-data">{this.props.lastBackup.backup_type}</h4>
               </div>
-            </div>
-
-            <br />
-
-            <div className="row">
               <div className="col-xs-4">
                 <h3>Last Backup Timestamp</h3>
                 <h4 class="home-application-data">{this.props.lastBackup.last_backup_timestamp}</h4>
@@ -34,12 +21,20 @@ class LastBackupItem extends Component  {
                 <h3>Start Time</h3>
                 <h4 class="home-application-data">{this.props.lastBackup.start_time}</h4>
               </div>
+            </div>
+
+            <br />
+
+            <div className="row">
               <div className="col-xs-4">
                 <h3>Completion Time</h3>
-                <h3>{this.props.lastBackup.completion_time}</h3>
+                <h4 class="home-application-data">{this.props.lastBackup.completion_time}</h4>
+              </div>
+              <div className="col-xs-8">
+                <h3>Status</h3>
+                <h4 class="text-color-green">{this.props.lastBackup.status}</h4>
               </div>
             </div>
-            <p><h3>Status:</h3> <h4 class="color-green">{this.props.lastBackup.status}</h4></p>
             <br />
 
         </div>

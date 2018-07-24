6e203bfb0e04ea4f942491ece758be50468acf35
uddish <uddish@squadplatform.com>
2018-07-24 11:33:26 +0200

    added date time field

--- src/Components/RecoverBackup/RecoverBackupForm.js
+++ src/Components/RecoverBackup/RecoverBackupForm.js
@@ -1,5 +1,9 @@
 import React, {Component} from 'react';
 import {FormGroup, ControlLabel, FormControl, Button} from 'react-bootstrap';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+import 'react-datepicker/dist/react-datepicker.css';
+
 
 class RecoverBackupForm extends React.Component {
   constructor(props) {
@@ -8,15 +12,18 @@ class RecoverBackupForm extends React.Component {
       cluster_name: '',
       application_name: '',
       list_of_files: '',
+      selected_date: moment()
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleDateChange = this.handleDateChange.bind(this);
 
   }
 
   handleChange(event)  {
     const target = event.target;
+
     if(target.name === 'cluster_name')  {
       this.setState({cluster_name: event.target.value})
     }
@@ -26,12 +33,21 @@ class RecoverBackupForm extends React.Component {
     else if(target.name === 'list_of_files')  {
       this.setState({list_of_files: event.target.value})
     }
+    else if(target.name === 'start_date') {
+      this.setState({start_date: event.target.value})
+    }
+  }
+
+  handleDateChange(date)  {
+    this.setState({
+			selected_date: date
+		});
   }
 
   handleSubmit(event)  {
     console.log('CLUSTER NAME -> ' + this.state.cluster_name);
     console.log('APPLICATION NAME -> ' + this.state.application_name);
-    console.log('FILES NAME -> ' + this.state.list_of_files);
+    console.log('FILES NAME -> ' + this.state.selected_date);
     event.preventDefault();
   }
 
@@ -76,6 +92,14 @@ class RecoverBackupForm extends React.Component {
               <option value="option_4">Option 4</option>
             </FormControl>
           </FormGroup>
+          <h5 className="text-margin-left">Recovery Date and Time</h5>
+          <DatePicker
+            className="date-picker-input"
+            selected={this.state.selected_date}
+            onChange={this.handleDateChange}
+            showTimeSelect
+            dateFormat="LLL"
+          />
           <Button type="submit" className="text-margin-left" bsStyle="primary">Submit</Button>
         </form>
         <br />
